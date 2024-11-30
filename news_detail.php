4ae0c29a566c21dba893d53a38583e6f1a0e44fe
azzazhr <azzahraqina70@gmail.com>
2024-11-30 11:42:07 +0700

    update detail

--- news_detail.php
+++ news_detail.php
@@ -2,39 +2,10 @@
 require 'config/database.php';
 $id = new MongoDB\BSON\ObjectId($_GET['id']);
 $article = $newsCollection->findOne(['_id' => $id]);
+include 'page/header.php';
+include 'page/sidebar.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $article['title'] ?></title>
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-    <!-- Navbar -->
-    <nav class="navbar">
-        <div class="navbar-container">
-            <a href="#" class="navbar-brand">Berita.com</a>
-            <ul class="navbar-links">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </div>
-    </nav>
-
-    <!-- Sidebar -->
-    <div class="sidebar">
-        <ul class="sidebar-links">
-            <li><a href="#">Dashboard</a></li>
-            <li><a href="#">News</a></li>
-            <li><a href="#">Settings</a></li>
-        </ul>
-    </div>
 
     <div class="container">
         <h1 class="my-4"><?= $article['title'] ?></h1>
@@ -58,10 +29,6 @@ $article = $newsCollection->findOne(['_id' => $id]);
         <p><?= nl2br($article['content']) ?></p>
     </div>
     </div>
-    <!-- Footer -->
-    <div class="footer">
-        &copy; 2024 Berita.com. All Rights Reserved
-    </div>
-</body>
-
-</html>
+<?php
+include 'page/footer.php';
+?>
