fdf82a3dc63743c0006e68ea2ceedf394a370cb9
dedybayu <dedybayusetiawan5@gmail.com>
2024-12-03 14:53:09 +0700

    update: session

--- config/auth.php
+++ config/auth.php
@@ -1,7 +1,11 @@
 <?php
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
 if (!isset($_SESSION['admin_logged_in']) || !$_SESSION['admin_logged_in']) {
     header('Location: login.php');
     exit;
 }
+
 ?>
