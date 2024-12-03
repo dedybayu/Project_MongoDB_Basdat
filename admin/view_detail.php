0bd3b41a152eca911813c00c8d90ebedcf0a4d66
azzazhr <azzahraqina70@gmail.com>
2024-12-03 13:35:42 +0700

    update

--- admin/view_detail.php
+++ admin/view_detail.php
@@ -27,6 +27,11 @@ include 'page/header.php';
     <div class="content-box">
         <p><?= nl2br($article['content']) ?></p>
     </div>
+    <!-- Tombol Edit dan Hapus -->
+    <div class="mt-4">
+        <a href="edit_news.php?id=<?= $article['_id'] ?>" class="btn btn-primary">Edit</a>
+        <a href="delete_news.php?id=<?= $article['_id'] ?>" class="btn btn-danger" onclick="return confirm('Apakah Anda yakin ingin menghapus berita ini?');">Hapus</a>
+    </div>
 </div>
 <?php
 include 'page/footer.php';
