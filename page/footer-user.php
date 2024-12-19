28f742e2aa0da87aa41e012654a814a900a5c841
dedybayu <dedybayusetiawan5@gmail.com>
2024-12-19 09:29:52 +0700

    update

--- page/footer-user.php
+++ page/footer-user.php
@@ -8,15 +8,14 @@
             <p>Jurusan Teknologi Informasi<br> Politeknik Negeri Malang</p>
         </div>
         <p class="mt-5 text-center">
-        &copy; <a class="font-weight-bold" href="index.php">KataData.com</a>. All Rights Reserved.
+            &copy; <a class="font-weight-bold" href="dashboard.php">KataData.com</a>. All Rights Reserved.
 
-        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-        Designed by <a class="font-weight-bold" href="https://htmlcodex.com">HTML Codex</a>
-    </p>
+            <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+        </p>
     </div>
-    
+
 </div>
- 
+
 <!-- Footer End -->
 
 
--- page/footer.php
+++ page/footer.php
@@ -5,9 +5,11 @@
         }
     </style>
 <!-- Footer -->
-<div class="footer">
-    <span>&copy; 2024 Berita.com. All Rights Reserved</span>
-</div>
+<p class="mt-5 text-center">
+        &copy; <a class="font-weight-bold" href="dashboard.php">KataData.com</a>. All Rights Reserved.
+
+        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+    </p>
 
 <!-- Bootstrap Bundle JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js">
